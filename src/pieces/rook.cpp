9ff680f52490c9e4f0b0ea6884811b7bdffad39a
jrosa <jrosa@student.agh.edu.pl>
2022-06-04 14:29:31 +0200

    Tweaked rook isMoveValid

--- src/pieces/rook.cpp
+++ src/pieces/rook.cpp
@@ -9,12 +9,11 @@ Rook::Rook(int x, int y, Color c): Piece(x, y , c){
 // TODO
 bool Rook::isValidMove(const Position & newPosition, Piece* (&board)[8][8]) const{
     // jesli sie nie zmieni pozycja -> sprawdzane w mainLoop
-
-    // jak zmieni sie x i y
-    if (this->getPosition().xPos != newPosition.xPos && this->getPosition().yPos != newPosition.yPos)
-        return false;
-
     Position relPosition = newPosition - this->getPosition();
+
+    // jak zmieni sie x i y (linia prosta)
+    if (relPosition.xPos != 0 && relPosition.yPos != 0)
+        return false;
     /**
      * Blok kodu sprawdza przypadki czy
      * 1) w której linii znajduje się nowa pozycja w odniesieniu do pionka
@@ -25,12 +24,12 @@ bool Rook::isValidMove(const Position & newPosition, Piece* (&board)[8][8]) cons
     if(relPosition.xp()){
         std::cout << "----xp-----" << std::endl;
 
-        for(int i = 1; i < relPosition.yPos; i++){
+        for(int i = 1; i < relPosition.xPos; i++){
             std::cout << "Petla sie wykonuje\n";
-            std::cout << "board[" << (this->getPosition() + Position(i, 0)).yPos;
-            std::cout << "][" << (this->getPosition() + Position(i, i)).xPos << "]\n";
+            std::cout << "board[" << (this->getPosition()).yPos;
+            std::cout << "][" << (this->getPosition() + Position(i, 0)).xPos << "]\n";
 
-            if(board[(this->getPosition() + Position(i, 0)).yPos][(this->getPosition() + Position(i, i)).xPos] != nullptr)
+            if(board[this->getPosition().yPos][(this->getPosition() + Position(1, 0)).xPos] != nullptr)
                 return false;
         }
     }
@@ -38,11 +37,11 @@ bool Rook::isValidMove(const Position & newPosition, Piece* (&board)[8][8]) cons
     if(relPosition.xm()){
         std::cout << "----xm-----" << std::endl;
 
-            for(int i = -1; i > relPosition.yPos; i--){
+            for(int i = -1; i > relPosition.xPos; i--){
                 std::cout << "Petla sie wykonuje\n";
-                std::cout << "board[" << (this->getPosition() + Position(i, 0)).yPos;
-                std::cout << "][" << (this->getPosition() + Position(i, i)).xPos << "]\n";
-                if(board[(this->getPosition() + Position(i, 0)).yPos][(this->getPosition() + Position(i, i)).xPos] != nullptr)
+                std::cout << "board[" << (this->getPosition()).yPos;
+                std::cout << "][" << (this->getPosition() + Position(i, 0)).xPos << "]\n";
+                if(board[this->getPosition().yPos][(this->getPosition() + Position(i, 0)).xPos] != nullptr)
                     return false;
             }
         }
@@ -50,12 +49,12 @@ bool Rook::isValidMove(const Position & newPosition, Piece* (&board)[8][8]) cons
     if(relPosition.yp()){
         std::cout << "----yp-----" << std::endl;
 
-            for(int i = 1; i < relPosition.xPos; i++){
+            for(int i = 1; i < relPosition.yPos; i++){
                 std::cout << "Petla sie wykonuje\n";
                 std::cout << "board[" << (this->getPosition() + Position(0, i)).yPos;
-                std::cout << "][" << (this->getPosition() + Position(i, i)).xPos << "]\n";
+                std::cout << "][" << (this->getPosition()).xPos << "]\n";
 
-                if(board[(this->getPosition() + Position(0, i)).yPos][(this->getPosition() + Position(i, i)).xPos] != nullptr)
+                if(board[(this->getPosition() + Position(0, i)).yPos][this->getPosition().xPos] != nullptr)
                     return false;
             }
         }
@@ -63,12 +62,12 @@ bool Rook::isValidMove(const Position & newPosition, Piece* (&board)[8][8]) cons
     if(relPosition.ym()){
         std::cout << "----ym-----" << std::endl;
 
-            for(int i = -1; i > relPosition.xPos; i--){
+            for(int i = -1; i > relPosition.yPos; i--){
                 std::cout << "Petla sie wykonuje\n";
                 std::cout << "board[" << (this->getPosition() + Position(0, i)).yPos;
-                std::cout << "][" << (this->getPosition() + Position(i, i)).xPos << "]\n";
+                std::cout << "][" << (this->getPosition()).xPos << "]\n";
 
-                if(board[(this->getPosition() + Position(0, i)).yPos][(this->getPosition() + Position(i, i)).xPos] != nullptr)
+                if(board[(this->getPosition() + Position(0, i)).yPos][(this->getPosition()).xPos] != nullptr)
                     return false;
             }
         }
