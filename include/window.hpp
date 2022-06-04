a2f5247a682c24ed82d6a30fd12b2cd9c281480c
StecBartlomiej <stecb@student.agh.edu.pl>
2022-06-04 20:45:30 +0200

    deleted old lib files

--- include/window.hpp
+++ include/window.hpp
@@ -1,9 +1,10 @@
 #ifndef CHESS_BOARD_HPP
 #define CHESS_BOARD_HPP
+
 #define SDL_MAIN_HANDLED
 
-#include "SDL2/SDL.h"
-#include <SDL2/SDL_image.h>
+#include "SDL.h"
+#include <SDL_image.h>
 #include <string>
 
 
